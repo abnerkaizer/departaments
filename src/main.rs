1e04239e87b5b00440072fee3c63d1128cf860cb
Abner Kaizer <abnerkaizer@protonmail.com>
2020-12-07 23:00:00 -0300

    More files and fmt.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::io;
 
 fn main() {
-    let mut empls: HashMap<String,String> = HashMap::new();
+    let mut empls: HashMap<String, String> = HashMap::new();
     loop {
         println!(
             "Menu:
@@ -28,51 +28,50 @@ fn main() {
         };
     }
 }
-fn add_empl(empls: &mut HashMap<String,String>,emp: &mut String,dep: &mut String,) {
+fn add_empl(empls: &mut HashMap<String, String>, emp: &mut String, dep: &mut String) {
     io::stdin().read_line(emp).expect("Employee");
 
     io::stdin().read_line(dep).expect("Department");
 
-    empls.insert(emp.to_string(),dep.to_string());
-
+    empls.insert(emp.to_string(), dep.to_string());
 }
-fn add_dep(empls: &mut HashMap<String,String>, dep: &mut String) {
+fn add_dep(empls: &mut HashMap<String, String>, dep: &mut String) {
     io::stdin().read_line(dep).expect("Department");
-    for (_key,val) in empls.iter() {
-        if val==dep {
-            println!("Department {} already existent.",dep);
+    for (_key, val) in empls.iter() {
+        if val == dep {
+            println!("Department {} already existent.", dep);
         }
     }
 }
-fn retrive_by_dep(empls: &HashMap<String,String>, dep: &mut String) {
+fn retrive_by_dep(empls: &HashMap<String, String>, dep: &mut String) {
     io::stdin().read_line(dep).expect("Department");
-    let mut emps:Vec<String> = Vec::new();
-    for (key,val) in empls.iter() {
-        if val==dep {
-            emps.push(key.to_string());   
+    let mut emps: Vec<String> = Vec::new();
+    for (key, val) in empls.iter() {
+        if val == dep {
+            emps.push(key.to_string());
         }
     }
     emps.sort();
-    
-    if emps.is_empty(){
+
+    if emps.is_empty() {
         println!("Is Empty or don't exist.");
-    }else {
-        for e in emps.iter(){
-            println!("Employee: {}", e);  
+    } else {
+        for e in emps.iter() {
+            println!("Employee: {}", e);
         }
     }
 }
-fn retrive_all(empls: &HashMap<String,String>) {
-    let mut emps:Vec<String> = Vec::new();
-    for (key,_val) in empls.iter() {
+fn retrive_all(empls: &HashMap<String, String>) {
+    let mut emps: Vec<String> = Vec::new();
+    for (key, _val) in empls.iter() {
         emps.push(key.to_string());
     }
     emps.sort();
     for e in emps {
-        let val:String = match empls.get(&e) {
+        let val: String = match empls.get(&e) {
             Some(val) => val.to_string(),
             None => "".to_string(),
         };
-        println!("Employee: {} Department: {} ", e,val);
+        println!("Employee: {} Department: {} ", e, val);
     }
 }
